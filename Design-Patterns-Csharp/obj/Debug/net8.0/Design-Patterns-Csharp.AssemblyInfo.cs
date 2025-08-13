dbf2dd806a5a7f68424d33c32bf4177b4eda9388
EmmanuelGambarte <emmanuelgambarte@gmail.com>
2025-08-12 23:52:53 -0300

    Fix

--- Design-Patterns-Csharp/obj/Debug/net8.0/Design-Patterns-Csharp.AssemblyInfo.cs
+++ Design-Patterns-Csharp/obj/Debug/net8.0/Design-Patterns-Csharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Design-Patterns-Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290963aab4c53c7b3118a4db3d287d2a5ab3039f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d4dba4972d65931905912e3247e75effba92a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Design-Patterns-Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Design-Patterns-Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
